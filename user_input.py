7c8deb28c7f0adbca41034e670dd3f34ebdd767e
KenNjugunaG <njugunakengachoki@gmail.com>
2024-10-31 15:30:09 +0300

    Add user_input.py to collect user input

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your Name: ")
+age = input("What is your Age: ")
+location = input("What is your Location: ")
+
+
+print(f"Hello my name is {name}, I am {age} years old, and I live in {location}.")
